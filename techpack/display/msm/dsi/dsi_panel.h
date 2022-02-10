3fe0f1eab067415e8b271e96d8de8a9633c76d43
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:08:04 +0200

    techpack: display: dsi_panel: read FOD dimming LUT
    Change-Id: I23f7914ce0adf6c1942c09212516ed5f6b22d289

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -307,6 +307,12 @@ enum touch_state {
 	TOUCH_LOW_POWER_STATE,
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u16 brightness;
+	u8 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -401,6 +407,9 @@ struct dsi_panel {
 	int paramNum;
 	int dc_state;
 	struct msm_param_info curDCModeParaInfo;
+
+	struct brightness_alpha_pair *fod_dim_lut;
+	unsigned int fod_dim_lut_len;
 };
 
 bool dsi_display_all_displays_dead(void);
